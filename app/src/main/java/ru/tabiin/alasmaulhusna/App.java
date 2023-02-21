8b39b22957728afa3c0ad9d75d4c44e96fb3861f
09702 <Raf_0707@mail.ru>
2023-02-21 18:58:43 +0300

    create navHost destroy

---
+++ app/src/main/java/ru/tabiin/alasmaulhusna/App.java
@@ -0,0 +1,42 @@
+package ru.tabiin.alasmaulhusna;
+
+import android.app.Application;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.google.android.material.color.DynamicColors;
+
+import ru.tabiin.alasmaulhusna.util.SharedPreferencesUtils;
+
+public class App extends Application {
+    private static App instance = null;
+
+    public static App getInstance() {
+        if (instance == null)
+            instance = new App();
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        int nightMode = SharedPreferencesUtils.getInteger(this, "nightMode");
+
+        switch (nightMode) {
+            case 0:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case 1:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case 2:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+        }
+
+        if (!SharedPreferencesUtils.getBoolean(this, "useDynamicColors")) {
+            DynamicColors.applyToActivitiesIfAvailable(this);
+        }
+    }
+}
--- app/src/main/java/ru/tabiin/alasmaulhusna/MainActivity.java
+++ app/src/main/java/ru/tabiin/alasmaulhusna/MainActivity.java
@@ -2,10 +2,19 @@ package ru.tabiin.alasmaulhusna;
 
 import static android.graphics.Color.BLACK;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import ru.tabiin.alasmaulhusna.R;
 import ru.tabiin.alasmaulhusna.databinding.ActivityMainBinding;
@@ -13,14 +22,19 @@ import ru.tabiin.alasmaulhusna.ui.about_app.AppAboutFragment;
 import ru.tabiin.alasmaulhusna.ui.counter.AllahNamesCounterFragment;
 import ru.tabiin.alasmaulhusna.ui.names.AllahNamesFragment;
 import ru.tabiin.alasmaulhusna.ui.names.AllahNamesInfoFragment;
+import ru.tabiin.alasmaulhusna.util.SharedPreferencesUtils;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
-    AllahNamesFragment allahNamesFragment;
-    AllahNamesInfoFragment allahNamesInfoFragment;
-    AllahNamesCounterFragment allahNamesCounterFragment;
-    AppAboutFragment appAboutFragment;
+    private AllahNamesFragment allahNamesFragment;
+    private AllahNamesInfoFragment allahNamesInfoFragment;
+    private AllahNamesCounterFragment allahNamesCounterFragment;
+    private AppAboutFragment appAboutFragment;
+
+    private boolean isEditing;
+
+    private NavHostFragment navHostFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if (!SharedPreferencesUtils.getBoolean(this, "isFirstOpen")) {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(new ContextThemeWrapper(this, R.style.Theme_AlAsmaUlHusna));
+            builder.setIcon(R.drawable.icon_tabiin_light);
+            builder.setTitle(R.string.welcome);
+            builder.setMessage(R.string.welcome_message);
+            builder.setPositiveButton("OK", (dialog, which) -> SharedPreferencesUtils.saveBoolean(this, "isFirstOpen", true));
+            builder.show();
+
+            navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.navHostFragment);
+            assert navHostFragment != null;
+            NavController navController = navHostFragment.getNavController();
+            //BottomNavigationView bottomNav = findViewById(R.id.bottomAppBar);
+            NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        }
 
         allahNamesFragment = new AllahNamesFragment();
         allahNamesInfoFragment = new AllahNamesInfoFragment();
@@ -37,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.containerFragment, allahNamesFragment).commit();
 
+
+
+        /*
         binding.bottomNavView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.listAllahNames:
@@ -61,5 +94,34 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
+
+         */
+
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        int id = navHostFragment.getNavController().getCurrentDestination().getId();
+        //if (!(id == R.id.devicesFragment || id == R.id.deviceSettingsFragment)) {
+            //actionBar.setDisplayHomeAsUpEnabled(false);
+        //}
+    }
+
+    @Override
+    public void recreate() {
+        getWindow().setWindowAnimations(R.style.WindowAnimationFadeInOut);
+        super.recreate();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
